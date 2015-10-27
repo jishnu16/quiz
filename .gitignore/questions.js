7e1f022a2b6f6c906b7adeacd684ac32c04b6489
prasunkp <prasunkp@INprasunkp.local>
2015-10-27 14:30:14 +0530

    css, scripts and html file is modified. Basic working app is made. But still there are many problems.

--- .gitignore/questions.js
+++ .gitignore/questions.js
@@ -4,4 +4,28 @@ var questions=[	'What is the capital of india?',
 				'what is 2^3',
 				'what is the full name of cd?'];
 
-exports.questions = questions;
+
+
+
+
+var show = function(){
+	var str = "<table align='center' cellpadding='10px'";
+	for(var i = 0;i < questions.length; i++)
+		str = str + "<tr> <td>"+ questions[i] +"</td>"+
+						"<td><input type='text' id='answer-" + i +"' /></td>"+
+						"</tr>";
+	str += "<tr>"+
+				"<td></td>"+
+				"<td><input type='button' value='Finish' class='button' onclick='checkAnswer()'></td>"+
+			"</tr>"+
+			"</table>"; 
+		document.getElementById('questions').innerHTML = str;
+
+}
+
+var showQuestion = function(){
+	document.getElementById('start').hidden = true;
+	show();
+	document.getElementById('questions').hidden = false;
+}
+
--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,8 @@ var storeTheName = function(){
 	document.getElementById('start').hidden = false;
 	document.getElementById('submitButton').hidden = true;
 	document.getElementById('name').hidden = true;
-
 	document.getElementById('showName').innerText = name;
-
 	document.getElementById('showName').innerHTML = 'Welcome ' + name;	
 }
 
-var showQuestion = function(){
-	document.getElementById('questions').hidden = false;
-}
+
